ae0a0d2c75b519c90b4c5705a47752640a885b77
Tiago Santos <84021366+Santuuus@users.noreply.github.com>
2023-03-20 21:12:59 +0000

    Now outputs the table name, database and number of rows

--- database_finder.py
+++ database_finder.py
@@ -1,7 +1,8 @@
 import os
 import sqlite3
-import binascii
+import csv
 import sys
+import binascii
 
 # Get the directory path from the command-line argument
 if len(sys.argv) != 2:
@@ -15,26 +16,40 @@ database_files = []
 # Walk through the directory and its subdirectories
 for root, dirs, files in os.walk(directory):
     for file in files:
-        # Try to connect to the file as a SQLite database
-        try:
-            with open(os.path.join(root, file), 'rb') as f:
-                header = f.read(16)
-                if header.startswith(b'SQLite format'):
-                    conn = sqlite3.connect(os.path.join(root, file))
-                    cursor = conn.cursor()
-                    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-                    # If we got here without an exception, the file is a SQLite database
-                    database_files.append(os.path.join(root, file))
-                    cursor.close()
-                    conn.close()
-        except (sqlite3.DatabaseError, IOError, binascii.Error):
-            # If we got here, the file is not a SQLite database
-            pass
+        # Check if the file is an SQLite database
+        with open(os.path.join(root, file), 'rb') as f:
+            header = f.read(100)
+            if b'SQLite format 3' in header:
+                database_files.append(os.path.join(root, file))
 
-# Print the database file paths
-if database_files:
-    print("The following files are SQLite databases:")
-    for file in database_files:
-        print(file)
-else:
-    print("No SQLite databases found in the directory.")
+# A list to store the table information
+table_info = []
+
+# Process each database file
+for database_file in database_files:
+    # Connect to the database
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+
+    # Get the table names and row counts
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = [row[0] for row in cursor.fetchall()]
+
+    for table in tables:
+        cursor.execute("SELECT COUNT(*) FROM " + table + ";")
+        row_count = cursor.fetchone()[0]
+        table_info.append((database_file, table, row_count))
+
+    # Close the database connection
+    conn.close()
+
+# Write the table information to a CSV file
+with open("database_finder.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Database File", "Table Name", "Row Count"])
+    for info in table_info:
+        writer.writerow(info)
+
+# Print the table information
+for info in table_info:
+    print("Database file: {}, Table name: {}, Row count: {}".format(info[0], info[1], info[2]))
